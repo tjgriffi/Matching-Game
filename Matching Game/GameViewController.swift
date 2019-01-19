709cca5aa102ea74ecf97b4f4879b7072446be4d
tjgriffi <tjgriffi357@gmail.com>
2019-01-18 18:13:20 -0800

    Added a reset button and end game screen for when you lose the game

--- Matching Game/GameViewController.swift
+++ Matching Game/GameViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+protocol endGameDelegate: class {
+    func restartGameButtonPressed()
+}
+
+class endGameView: UIView{
+    weak var delegate: endGameDelegate?
+
+    
+    required init(frame: CGRect, pDelegate: endGameDelegate){
+        super.init(frame: frame);
+        self.delegate = pDelegate;
+        
+        self.backgroundColor = UIColor.red;
+        
+        let restartButtonFrame = CGRect(x: self.frame.width/2, y: self.frame.height/4, width: 50, height: 50);
+        let restartButton = UIButton(frame: restartButtonFrame );
+        restartButton.backgroundColor = UIColor.blue;
+        restartButton.addTarget(self, action: #selector(restartGame), for: .touchUpInside);
+        
+        self.addSubview(restartButton);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func restartGame(sender: UIButton) {
+        delegate?.restartGameButtonPressed();
+    }
+}
+
+class GameViewController: UIViewController, endGameDelegate {
     
     // Variables
     var timer = Timer();
@@ -19,6 +50,9 @@ class GameViewController: UIViewController {
     
     // Array that holds the players move
     var playerMoves: [IntegerLiteralType] = [];
+    
+    // Endgame popup
+    var endGamePopUp: UIView!;
     
     // Properties
     @IBOutlet var redButton: UIButton!
@@ -31,9 +65,14 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Hide the endgame popup
+//        endGamePopUp.isHidden = true;
+        
+        // Add all of the buttons to an array to keep track of them
         buttons = [redButton, greenButton, blueButton, yellButton];
         
-//        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.gameplay), userInfo: nil, repeats: true);
+        // create the endgame popup
+        self.createTheEndGamePopUp();
         
         self.gameplay();
 
@@ -44,13 +83,31 @@ class GameViewController: UIViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
-//    @objc func highlightButton(button: UIButton) {
-//
-//        button.isHighlighted = true;
-//        highlightTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false){_ in
-//            button.isHighlighted = false;
-//        }
-//    }
+    // Create the endgame pop up
+    func createTheEndGamePopUp() {
+        
+        let endGameViewFrame = CGRect.init(x: CGFloat(view.frame.width/4), y: CGFloat(view.frame.height/4), width: CGFloat(view.frame.width/2), height: view.frame.height/4);
+        endGamePopUp = endGameView(frame: endGameViewFrame, pDelegate: self);
+        
+        self.view.addSubview(endGamePopUp);
+                
+        endGamePopUp.isHidden = true;
+        
+    }
+    
+    @objc func restartGameButtonPressed() {
+        // Clear the order array
+        order = [];
+        
+        // Hide the endgame pop up
+        endGamePopUp.isHidden = true;
+        
+        // Run the gameplay sequence again
+        self.gameplay();
+        
+        
+        print("Restart was pressed!!!");
+    }
     
     // Takes the order of the inputs and displays them
     @objc func displayOrder(buttonOrder: [Int]){
@@ -67,30 +124,13 @@ class GameViewController: UIViewController {
             }
         }
         
-//        timer = Timer.scheduledTimer(withTimeInterval: currOffset, repeats: false){_ in
-//            self.turnOnButtons();
-//        }
     }
     
     //MARK: Actions
     @IBAction func colorButtonPressed(_ sender: UIButton) {
         
-        if(sender == redButton)
-        {
-            playerMoves.append(0);
-        }
-        else if(sender == greenButton)
-        {
-            playerMoves.append(1);
-        }
-        else if(sender == blueButton)
-        {
-            playerMoves.append(2);
-        }
-        else if(sender == yellButton)
-        {
-            playerMoves.append(3);
-        }
+        // Add the button(index of the button) that was pressed to the moveset
+        playerMoves.append(buttons.firstIndex(of: sender)!);
         
         // Turn off the buttons for further interactions and start the calculations to see if the player input the correct moves
         if(playerMoves.count >= order.count)
@@ -99,6 +139,7 @@ class GameViewController: UIViewController {
             playerTurn();
         }
     }
+    
     
     // Contains the main play cycle for the game
     @objc func gameplay() {
@@ -122,7 +163,8 @@ class GameViewController: UIViewController {
         if playerMoves.count == order.count {
             for i in 0..<order.count{
                 if playerMoves[i] != order[i]{
-                    print("Wrong answer!!!!")
+//                    print("Wrong answer!!!!")
+                    loadEndGameVew();
                     return;
                 }
             }
@@ -132,14 +174,12 @@ class GameViewController: UIViewController {
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        print("Will disappear");
-        self.timer.invalidate();
+        timer.invalidate();
         highlightTimer.invalidate();
     }
     
     // Turn off the buttons
     func turnOffButtons() {
-//        print("Here");
         redButton.isEnabled = false;
         blueButton.isEnabled = false;
         greenButton.isEnabled = false;
@@ -148,11 +188,16 @@ class GameViewController: UIViewController {
     
     // Turn on the buttons
     func turnOnButtons() {
-        print("Here");
         redButton.isEnabled = true;
         blueButton.isEnabled = true;
         greenButton.isEnabled = true;
         yellButton.isEnabled = true;
+    }
+    
+    func loadEndGameVew(){
+//        print("The endgame popup should appear");
+        endGamePopUp.isHidden = false;
+    
     }
 
 }
